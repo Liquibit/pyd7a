4769ff375d92b471721116aeaa5d7b75500458f3
Liam Oorts <liam.oorts@aloxy.io>
2022-12-07 21:05:04 +0100

    logging should have the option to log to a file

--- util/logger.py
+++ util/logger.py
@@ -20,10 +20,13 @@ import logging
 
 # configure the logger when using the supplied examples, as opposed to running as a library
 # called from an external program (which then should take care of configuring the logging)
-def configure_default_logger(is_verbose_enabled, logging_level = logging.INFO):
+def configure_default_logger(is_verbose_enabled, file = None, logging_level = logging.INFO):
   log = logging.getLogger()
   formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-  handler = logging.StreamHandler()
+  if file:
+    handler = logging.FileHandler(file)
+  else:
+    handler = logging.StreamHandler()
   handler.setFormatter(formatter)
   log.addHandler(handler)
   log.setLevel(logging_level)
